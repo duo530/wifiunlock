b83c00057daeee43459bb19dd6e7c5bd1b353f63
duo530 <cavaluslg@gmail.com>
2024-10-20 17:13:15 -0300

    Update unlock.ps1

--- unlock.ps1
+++ unlock.ps1
@@ -1,8 +1,8 @@
 netsh wlan show profile | Select-String "All User Profile" | ForEach-Object { 
     $profile = $_.ToString().Split(":")[1].Trim(); 
-    $key = netsh wlan show profile name=$profile key=clear | Select-String "Key Content" 
+    $key = netsh wlan show profile name="$profile" key=clear | Select-String "Key Content" 
     if ($key) { 
         $password = $key.ToString().Split(":")[1].Trim(); 
         "$profile : $password" 
     } 
-} | Out-File -FilePath "keys.txt"
+} | Out-File -FilePath "keys.txt" -Encoding utf8
